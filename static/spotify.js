e7e6bb0e56ed7a737db12f9ae3817fd9f261f36e
Usmaan Wahab <usmaanwahab11@outlook.com>
2025-08-28 13:01:58 +0100

    changed spotify layout to remove need for buttons

--- static/spotify.js
+++ static/spotify.js
@@ -1,11 +1,16 @@
 function fetchTopArtists() {
   fetch("/spotify/top/artists/long_term?limit=50&offset=0")
     .then(r => r.text())
-    .then(html => document.getElementById("spotify-content").innerHTML = html)
+    .then(html => document.getElementById("top-artists").innerHTML = html)
 }
 
 function fetchTopTracks() {
   fetch("/spotify/top/tracks/long_term?limit=50&offset=0")
     .then(r => r.text())
-    .then(html => document.getElementById("spotify-content").innerHTML = html)
+    .then(html => document.getElementById("top-tracks").innerHTML = html)
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchTopTracks();
+  fetchTopArtists();
+})
